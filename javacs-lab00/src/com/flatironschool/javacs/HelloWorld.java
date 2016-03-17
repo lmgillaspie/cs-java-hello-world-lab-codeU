fef47a5c6aeeb4aae8b0cdfca4c3fad773cf95cd
Lindsey M. Gillaspie <lindsey.gillaspie@gmail.com>
2016-03-17 13:28:40 -0500

    Finito

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,11 +1,16 @@
 package com.flatironschool.javacs;
+//Edited by Lindsey Gillaspie  3/17/2016
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+        String version = System.getProperty("java.specification.version");
+	Double dblVersion = Double.parseDouble(version);
+	return dblVersion;
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
+	Double finalVersion = getVersion();
+	System.out.println("You have java version " + finalVersion);
     }
 }
